ae41b33fe645183dd1f4d24a835783d997bc99ab
raffibromabar-web <raffibromabar@gmail.com>
2026-03-31 09:20:18 +0700

    menambahkan teks daftar di menu

--- looping.cpp
+++ looping.cpp
@@ -6,7 +6,13 @@ int angka;
 int pilihan;
 
 void tampilMenu() {
+    cout << "\n=======================================" << endl;
+    cout << "  PROGRAM PENGECEKAN ANGKA (PRIMA & FIBONACCI)" << endl;
+    cout << "=======================================" << endl;
+    cout << "1. Cek Bilangan Prima" << endl;
+    cout << "2. Cek Bilangan Fibonacci" << endl;
+    cout << "0. Keluar" << endl;
+    cout << "Masukkan pilihan Anda: ";
 
-    
 }
 
